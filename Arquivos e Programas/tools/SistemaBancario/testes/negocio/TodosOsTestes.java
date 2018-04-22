327fd4b1866c45acae0a01d73474480f666e6152
Emerson\House <emersonline2007@gmail.com>
2018-04-22 14:33:07 -0300

    Materiais do Curso de Especialista em Teste de Software Completo.

---
+++ Arquivos e Programas/tools/SistemaBancario/testes/negocio/TodosOsTestes.java
@@ -0,0 +1,44 @@
+package negocio;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+//Colocar todas as classes de teste no SuiteClasses
+@SuiteClasses({ GerenciadoraClientesTest_Ex1.class, GerenciadoraClientesTest_Ex2.class,
+	GerenciadoraClientesTest_Ex3.class, GerenciadoraClientesTest_Ex4.class, GerenciadoraClientesTest_Ex5.class,
+	GerenciadoraClientesTest_Ex7.class, GerenciadoraClientesTest_Ex8.class, GerenciadoraClientesTest_Ex10.class,
+	GerenciadoraContasTest_Ex3.class, GerenciadoraContasTest_Ex4.class, GerenciadoraContasTest_Ex6.class,
+	GerenciadoraContasTest_Ex11.class })
+
+public class TodosOsTestes {
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+  @SuiteClasses({ GerenciadoraClientesTest_Ex1.class, GerenciadoraClientesTest_Ex2.class,
+	GerenciadoraClientesTest_Ex3.class, GerenciadoraClientesTest_Ex4.class, GerenciadoraClientesTest_Ex5.class,
+	GerenciadoraClientesTest_Ex7.class, GerenciadoraClientesTest_Ex8.class, GerenciadoraContasTest_Ex3.class,
+	GerenciadoraContasTest_Ex4.class, GerenciadoraContasTest_Ex6.class})
+ */
+
+
+
+
